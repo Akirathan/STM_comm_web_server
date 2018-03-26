aace27c7c4560f9ca293e34b010b6a9676a5d4c3
mayfa <pavel.akira.marek@gmail.com>
2018-03-26 17:27:11 +0200

    Add actual item update functionality
    STM periodically updates via this function

--- stm_comm/models.py
+++ stm_comm/models.py
@@ -25,6 +25,7 @@ class Device(models.Model):
 
 class Item(models.Model):
     device = models.ForeignKey('Device', on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
     type = models.CharField(max_length=10)
     value = models.CharField(max_length=30)
     time = models.DateTimeField()
--- stm_comm/views.py
+++ stm_comm/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest
+from django.utils import timezone
 from .connection_manager import ConnectionManager
-from .models import Device
+from .models import Device, Item
 
 
-# Create your views here.
-def actual(request: HttpRequest, actual_item: str) -> HttpResponse:
-    print('in view, actual_item=%s' % actual_item)
+def actual(request: HttpRequest, actual_item_name: str) -> HttpResponse:
+    """
+    Device periodically POSTs values via this URL.
+    :param request: must be POST type and include "value" key
+    :param actual_item_name: name of the item that device wants to update
+    :return:
+    """
+    if len(request.POST.dict()) == 0:
+        # Error: request is not POST type
+        return HttpResponse(status=400)
+
+    device_id = ConnectionManager.get_device_id(request.get_host(), request.get_port())
+    if device_id is None:
+        # Error: device not connected
+        return HttpResponse(status=400)
+
+    # Get device and corresponding item from the database
+    device = get_object_or_404(Device, pk=device_id)
+    item = get_object_or_404(device.item_set, name=actual_item_name)
+
+    if item.type != 'actual':
+        # Error: wrong item type
+        return HttpResponse(status=400)
+
+    # Parse the value from POST request
+    try:
+        value = request.POST['value']
+    except KeyError:
+        # Error: wrong format of POST request
+        return HttpResponse(status=400)
+
+    # Insert the value in the database.
+    item.value = value
+    item.time = timezone.now()
+    item.save()
+
     return HttpResponse()
 
 
