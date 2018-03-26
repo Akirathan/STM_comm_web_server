9137f13a97f39696702afe33ebbb636ea7f88e36
mayfa <pavel.akira.marek@gmail.com>
2018-03-26 15:43:49 +0200

    Add models

--- stm_comm/models.py
+++ stm_comm/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Device(models.Model):
+    # online/offline
+    status = models.CharField(max_length=7)
+    fw_version = models.CharField(max_length=10)
+    # private DES key
+    key = models.CharField(max_length=8)
+
+
+class Item(models.Model):
+    device = models.ForeignKey('Device', on_delete=models.CASCADE)
+    type = models.CharField(max_length=10)
+    value = models.CharField(max_length=30)
+    time = models.DateTimeField()
