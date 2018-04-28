a407c5d6d0f9877248ec5c85657251304f7722b0
mayfa <pavel.akira.marek@gmail.com>
2018-04-28 12:33:30 +0200

    stm_comm-Models: add str representation

--- stm_comm/models.py
+++ stm_comm/models.py
@@ -92,6 +92,9 @@ class Time:
     def to_json(self) -> str:
         return '{"hours":%d,"minutes":%d}' % (self.hours, self.minutes)
 
+    def __str__(self):
+        return '%d:%d' % (self.hours, self.minutes)
+
 
 class Interval:
     def __init__(self, from_time: Time, to_time: Time, temp: int):
@@ -103,6 +106,9 @@ class Interval:
     def to_json(self) -> str:
         return '{"from":%s,"to":%s,"temp":%d}' % (self.from_time.to_json(), self.to_time.to_json(), self.temp)
 
+    def __str__(self):
+        return 'from: %s, to: %s, temp: %d' % (self.from_time, self.to_time, self.temp)
+
 
 class IntervalsItem(ConfigItem):
     device = models.ForeignKey('Device', default=None, on_delete=models.CASCADE)
@@ -113,6 +119,14 @@ class IntervalsItem(ConfigItem):
         self.intervals_num = 0
         self.value = '[]'
         return
+
+    def __str__(self):
+        retval = '['
+        for interval in self.__get_intervals__():
+            retval += interval.__str__()
+            retval += ','
+        retval += ']'
+        return retval
 
     def render(self) -> str:
         return render_to_string('items/intervals.html', {'interval_list': self.__get_intervals__()})
@@ -136,6 +150,8 @@ class IntervalsItem(ConfigItem):
         # Remove last array bracket
         self.value = self.value.replace(']', '')
 
+        if self.intervals_num >= 1:
+            self.value += ','
         self.value += interval.to_json()
         self.value += ']'
 
