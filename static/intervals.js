0682957de793fa86ab4eb57133b7be922273b1a7
mayfa <pavel.akira.marek@gmail.com>
2018-04-28 17:55:53 +0200

    Intervals template: add intervals changed functionality

--- static/intervals.js
+++ static/intervals.js
@@ -1,3 +1,5 @@
+var intervalsChanged = false;
+
 function editAll(event) {
     $(".interval").hide();
     $(".editable-interval").show();
@@ -16,9 +18,32 @@ function doneEditingAll(event) {
     target.html("Edit");
     target.off("click");
     target.on("click", editAll);
+
+    if (intervalsChanged) {
+        showSaveButton();
+    }
+}
+
+/**
+ * Show "save into device" button if some device values were changed.
+ */
+function showSaveButton() {
+    $("#save_to_device_group").show();
+}
+
+function intervalChanged(event) {
+    intervalsChanged = true;
+
+    var overviewInterval = getAssociatedOverviewInterval(event.target.id);
+    overviewInterval.html(event.target.value);
 }
 
 function deleteInterval(event) {
 
+}
+
+function getAssociatedOverviewInterval(editableIntervalId) {
+    var overviewIntervalId = editableIntervalId.replace("editable_", "");
+    return $("#" + overviewIntervalId);
 }
 
