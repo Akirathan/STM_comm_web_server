49dfd3476dc2312ebed44f41e103ed9880ade952
mayfa <pavel.akira.marek@gmail.com>
2018-05-01 18:37:40 +0200

    devices.js: add Device

--- static/devices.js
+++ static/devices.js
@@ -9,3 +9,31 @@ var deviceList = new class {
         }
     }
 };
+
+class Device {
+   constructor(deviceId) {
+        this._deviceId = deviceId;
+        this._configItems = [];
+    }
+
+    get id() {
+        return this._deviceId;
+    }
+
+    addConfigItem(configItem) {
+        if (configItem instanceof ConfigItem) {
+            this._configItems.push(configItem);
+        }
+    }
+
+    /**
+     * Saves every config item into device.
+     */
+    saveIntoDevice() {
+        for (let configItem of this._configItems) {
+            if (configItem.isChanged()) {
+                configItem.saveIntoDevice();
+            }
+        }
+    }
+}
