2b882d327cc6586e5d6cee92e69570b7b149b65a
mayfa <pavel.akira.marek@gmail.com>
2018-05-22 13:41:14 +0200

    Fix ConnectionManager
    also refactor its connected_devices static member

--- stm_comm/connection_manager.py
+++ stm_comm/connection_manager.py
@@ -1,15 +1,19 @@
 
 class ConnectionManager:
+    # Map device_id <-> remote_address (client's IP address)
     connected_devices = {}
 
     @staticmethod
-    def add_device(device_id: str, host: str, port: str):
+    def add_device(device_id: str, remote_addr: str):
         if device_id in ConnectionManager.connected_devices:
             return
-        ConnectionManager.connected_devices[device_id] = (host, port)
+        ConnectionManager.connected_devices[device_id] = remote_addr
+        print('ConnectionManager: device with id=' + device_id + ' connected at remote_addr=' + remote_addr)
         return
 
     @staticmethod
-    def get_device_id(host: str, port: str) -> str:
-        return
+    def get_device_id(remote_addr: str) -> str:
+        for (device_id_item, remote_addr_item) in ConnectionManager.connected_device.items():
+            if remote_addr_item == remote_addr:
+                return device_id_item
 
