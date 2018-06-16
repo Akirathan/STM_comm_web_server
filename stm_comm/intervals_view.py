e7cb4210526fbb0ef6cb75842c7ec62c960f4a35
mayfa <pavel.akira.marek@gmail.com>
2018-06-16 16:47:31 +0200

    intervals_view: use ConnectionManager.get_device_from_request

--- stm_comm/intervals_view.py
+++ stm_comm/intervals_view.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from .connection_manager import ConnectionManager
-from .models import Device
 
 
 @csrf_exempt
@@ -11,6 +8,8 @@ def get_intervals_timestamp(request: HttpRequest) -> HttpResponse:
     if request.method != 'GET':
         return HttpResponse(404)
 
-    device_id = ConnectionManager.get_device_id_from_request(request)
+    device = ConnectionManager.get_device_from_request(request)
+    if device is None:
+        return HttpResponse(404)
 
-    return HttpResponse()
+    return HttpResponse(device.get_intervals_timestamp())
