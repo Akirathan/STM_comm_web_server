c84bd64aadad4c3e5e184efb817b2741a2cba237
mayfa <pavel.akira.marek@gmail.com>
2018-06-21 18:15:03 +0200

    Fix urls - slash was prepended

--- stm_comm/urls.py
+++ stm_comm/urls.py
@@ -4,7 +4,7 @@ from . import views, intervals_view
 app_name = 'stm_comm'
 urlpatterns = [
     path('connect', views.connect, name='connect'),
-    path('actual/temp/', views.update_temp, name='temp'),
+    path('actual/temp', views.update_temp, name='temp'),
     path('config/intervals/timestamp', intervals_view.get_intervals_timestamp, name='get_intervals_timestamp'),
     path('config/intervals', intervals_view.get_or_post_intervals, name='get_or_post_intervals')
 ]
