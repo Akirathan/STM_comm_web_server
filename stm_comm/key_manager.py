d8debaf1e4806c64830246b5809e4dbf37006d8e
mayfa <pavel.akira.marek@gmail.com>
2018-07-05 12:35:52 +0200

    Add Key class

--- stm_comm/key_manager.py
+++ stm_comm/key_manager.py
@@ -4,6 +4,26 @@ from random import randrange
 from stm_comm.models import Device
 
 
+class Key:
+    def __init__(self, value):
+        if type(value) == str:
+            self._hex_str_value = value
+            self._bytes_value = bytes.fromhex(value)
+        elif type(value) == bytes:
+            self._bytes_value = value
+            self._hex_str_value = value.hex()
+        else:
+            raise ValueError('Unsupported type')
+
+    @property
+    def hex_str(self) -> str:
+        return self._hex_str_value
+
+    @property
+    def bytes(self) -> bytes:
+        return self._bytes_value
+
+
 class KeyManager:
     KEY_TIMEOUT_SECONDS = 4*60
 
