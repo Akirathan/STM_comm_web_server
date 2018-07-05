4168d25973cb755c0112a5ca577d1629f0d494dc
mayfa <pavel.akira.marek@gmail.com>
2018-07-05 13:55:41 +0200

    DesKey: add key length check to constructor

--- stm_comm/des_key.py
+++ stm_comm/des_key.py
@@ -1,10 +1,16 @@
 
 class DesKey:
+    KEY_LEN = 8
+
     def __init__(self, value):
         if type(value) == str:
+            if len(value) != DesKey.KEY_LEN * 2:
+                raise ValueError('Wrong key length')
             self._hex_str_value = value
             self._bytes_value = bytes.fromhex(value)
         elif type(value) == bytes:
+            if len(value) != DesKey.KEY_LEN:
+                raise ValueError('Wrong key length')
             self._bytes_value = value
             self._hex_str_value = value.hex()
         else:
