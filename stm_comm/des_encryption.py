31798afa44a7666e3911da5a08a48dfb24f23dbe
mayfa <pavel.akira.marek@gmail.com>
2018-07-06 10:18:14 +0200

    des_encryption.py: encrypt_response_body accepts also bytes and str arguments

--- stm_comm/des_encryption.py
+++ stm_comm/des_encryption.py
@@ -8,7 +8,13 @@ def decrypt_req_body(request: HttpRequest, key: DesKey) -> bytes:
     return des_obj.decrypt(request.body)
 
 
-def encrypt_response_body(key: DesKey, body: str) -> bytes:
+def encrypt_response_body(key: DesKey, body) -> bytes:
+    if type(body) == str:
+        body = bytes(body, 'ascii')
+    elif type(body) == bytes:
+        pass
+    else:
+        raise ValueError('Unsupported parameter type')
+
     des_obj = pyDes.des(key.bytes, mode=pyDes.ECB, pad='\0', padmode=pyDes.PAD_NORMAL)
-    body = bytes(body, 'ascii')
     return des_obj.encrypt(body)
