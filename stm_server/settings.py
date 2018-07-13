e12c9b9f4572359b4a25b181726628b4c97c09e4
mayfa <pavel.akira.marek@gmail.com>
2018-07-13 15:23:04 +0200

    Add local IPs to ALLOWED_HOSTS

--- stm_server/settings.py
+++ stm_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@46##7x7ncr2xp$m(2xmae^n%q!+)e(yic1^4dr0l@9k(na@@4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.1', '127.0.0.1']
 
 
 # Application definition
